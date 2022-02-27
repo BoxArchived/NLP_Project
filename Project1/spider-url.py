2940d5d6d8fccf66828fce95c48fb3a6f6e3e5f6
boxmars <2803726925@qq.com>
2022-02-27 22:24:50 +0800

    update

--- Project1/spider-url.py
+++ Project1/spider-url.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 def crawl_url(url: str) -> [(str, str)]:
     page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
+    soup = BeautifulSoup(page.content, "html.parser")
     t_book_list = soup.find_all('article', 'product_pod')
     t_book_list = [book.find_all('h3')[0].find_all('a')[0] for book in t_book_list]
     book_list = [(book['title'].replace('<','').replace('>','').replace('\\','').replace('/','').replace(':','').replace('*','').replace('?','').replace('<',''),'https://books.toscrape.com/catalogue/{}'.format(book['href'])) for book in t_book_list]
